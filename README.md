b56c8cb37328fbe8334ad535cff3049245894fb7
Piotr Kowalski <piecioshka@gmail.com>
2016-11-30 00:30:56 +0100

    Setup gallery mechanism

--- README.md
+++ README.md
@@ -13,8 +13,28 @@
 * jak ustawić kodowanie dokumentu
 * jak ustawić typ pliku
 * korzystania z serwisów zewnętrznych do pobierania obrazków
-* jak wybrać kolor z listy predefiniowanych w CSS (TODO: skąd je wziąć?)
 * jak dołączyć ładny font ze strony fonts.google.com (TODO: potrzebuje ładnych fontów)
+* definiowania atrybutów
+
+CSS
+
+* jak wybrać kolor z listy predefiniowanych w CSS (TODO: skąd je wziąć?)
+* selektorów razem z specifity
+* funkcji calc
+
+JavaScript
+* definiowanie zmiennych, funkcji, klas, stałych
+* czym jest konstruktor
+* co to jest DOM
+* jak tworzy się metody?
+* jak poczekać na załadowanie DOMa
+* co to są parametry vs. argumenty?
+* jak nasłuchiwać na zdarzenia UI?
+* jak dowiedzieć się w co user kliknął?
+* wzorzec: dependency injection
+* jak zarządzać klasami CSS elementów DOMa
+* metody prywatne
+* instrukcja switch
 
 ## Placeholders
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,132 @@
-console.log('main file');
+const GALLERY_SELECTOR = '.gallery';
+const DISPLAY_CONTAINER_SELECTOR = '.display-area';
+
+const ESCAPE_CODE = 27;
+const LEFT_CODE = 37;
+const RIGHT_CODE = 39;
+
+function buildImage(url) {
+    let $image = document.createElement('img');
+    $image.setAttribute('src', url);
+    return $image;
+}
+
+class Gallery {
+    constructor() {
+        console.log('Gallery was setup');
+        this.$gallery = document.querySelector(GALLERY_SELECTOR);
+    }
+
+    start(displayArea) {
+        this.$gallery.addEventListener('click', (evt) => {
+            let $image = evt.target;
+            displayArea.display($image);
+            evt.preventDefault();
+        });
+
+        document.addEventListener('keydown', (evt) => {
+            switch (evt.keyCode) {
+                case ESCAPE_CODE:
+                    displayArea.destroy();
+                    break;
+
+                case LEFT_CODE:
+                    displayArea.displayPrevPicture();
+                    break;
+
+                case RIGHT_CODE:
+                    displayArea.displayNextPicture();
+                    break;
+            }
+        })
+    }
+}
+
+class DisplayArea {
+    constructor() {
+        console.log('DisplayArea was setup');
+        this.$area = document.querySelector(DISPLAY_CONTAINER_SELECTOR);
+        this.$main = this.$area.parentNode;
+        this.$current = null;
+        this._setupArrows();
+    }
+
+    display($thumb) {
+        this.$current = $thumb;
+
+        // let thumbSource = $thumb.getAttribute('src');
+        let $link = $thumb.parentNode;
+        let fullSource = $link.getAttribute('href');
+
+        let $image = buildImage(fullSource);
+
+        this._show();
+        this._clear();
+        this._render($image);
+    }
+
+    displayPrevPicture() {
+        let $link = this.$current.parentNode;
+        let $li = $link.parentNode;
+        let $prevLi = $li.previousElementSibling;
+
+        if (!$prevLi) {
+            $prevLi = $li.parentNode.lastElementChild;
+        }
+
+        let $prevImage = $prevLi.querySelector('img');
+        this.display($prevImage);
+    }
+
+    displayNextPicture() {
+        let $link = this.$current.parentNode;
+        let $li = $link.parentNode;
+        let $nextLi = $li.nextElementSibling;
+
+        if (!$nextLi) {
+            $nextLi = $li.parentNode.firstElementChild;
+        }
+
+        let $nextImage = $nextLi.querySelector('img');
+        this.display($nextImage);
+    }
+
+    destroy() {
+        this._hide();
+        this._clear();
+    }
+
+    _setupArrows() {
+        console.log('_setupArrows');
+        this.$main.querySelector('.left-arrow').addEventListener('click', () => {
+            this.displayPrevPicture();
+        });
+        this.$main.querySelector('.right-arrow').addEventListener('click', () => {
+            this.displayNextPicture();
+        });
+    }
+
+    _clear() {
+        this.$area.innerHTML = '';
+    }
+
+    _render($image) {
+        this.$area.appendChild($image);
+    }
+
+    _show() {
+        this.$main.classList.remove('hide');
+    }
+
+    _hide() {
+        this.$main.classList.add('hide');
+    }
+}
+
+function bootstrap() {
+    let displayArea = new DisplayArea();
+    let gallery = new Gallery();
+    gallery.start(displayArea);
+}
+
+window.addEventListener('DOMContentLoaded', bootstrap);
